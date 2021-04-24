12a991f2334eafeddb68ff7279dbbc004bf4538f
Jennifer Azarow <JAzarow@azarowj.lan>
2021-04-24 18:30:32 -0400

    working on the challenge

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_data(browser)
     }
 
     # Stop webdriver and return data
@@ -101,3 +102,34 @@ if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
+def hemisphere_data(browser):
+
+    # 1. Use browser to visit the URL
+    url = 'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/index.html'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    hemi_html = browser.html
+    hemi_soup = soup(hemi_html, 'html.parser')
+    items = hemi_soup.find_all('div', class_='item')
+    for i in items:
+        url_to_image = i.find('a', class_='itemLink product-item')['href']
+        browser.visit(f'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/{url_to_image}')
+        img_html = browser.html
+        img_soup = soup(img_html, 'html.parser')
+        img_url_rel = img_soup.find('img', class_='wide-image').get('src')
+        img_url = f'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/{img_url_rel}'
+        title = img_soup.find('h2', class_='title').get_text()
+        hemisphere_image_urls.append({'img_url': img_url, 'title': title})
+        browser.visit(url)
+
+
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+
+if __name__ == "__main__":
+    return hemispheres
